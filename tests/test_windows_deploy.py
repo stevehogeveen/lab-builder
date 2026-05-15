d18bd1170ae8e3073439a2ed2a3453872603ac0d
stevehogeveen <steve.hogeveen@gmail.com>
2026-05-15 18:34:42 -0400

    Simplify shared setup UI patterns

--- tests/test_windows_deploy.py
+++ tests/test_windows_deploy.py
@@ -12,5 +12,8 @@ def test_build_standalone_vmx_uses_esxi_registerable_defaults():
     assert 'displayName = "win2022-01"' in vmx
     assert 'firmware = "efi"' in vmx
     assert 'efi.secureBoot.enabled = "TRUE"' in vmx
+    assert 'pciBridge4.virtualDev = "pcieRootPort"' in vmx
     assert 'scsi0:0.fileName = "win2022-01.vmdk"' in vmx
     assert 'ethernet0.networkName = "VM Network"' in vmx
+    assert "scsi0.pciSlotNumber" not in vmx
+    assert "ethernet0.pciSlotNumber" not in vmx
